6db9a6394452187397758057feb643eb37eea969
makewheels <makewheels>
2020-07-04 22:49:48 +0800

    暂时先这样

---
+++ src/main/java/com/eg/videoosandserver/VideoOsAndServerApplication.java
@@ -0,0 +1,13 @@
+package com.eg.videoosandserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoOsAndServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoOsAndServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eg/videoosandserver/VideoOsAndServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.videoosandserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoOsAndServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
