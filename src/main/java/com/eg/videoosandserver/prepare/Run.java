063c1a64dcce1af723542e4180432178332bc476
makewheels <makewheels>
2020-07-06 19:26:08 +0800

    progress

--- src/main/java/com/eg/videoosandserver/prepare/Run.java
+++ src/main/java/com/eg/videoosandserver/prepare/Run.java
@@ -34,7 +34,7 @@ public class Run {
      * @param videoId
      */
     private static void uploadToObjectStorage(MakeM3u8Result makeM3u8Result, String videoId) {
-        File m3u8File = makeM3u8Result.getM3u8File();
+        //准备对象存储前缀
         Calendar calendar = Calendar.getInstance();
         int year = calendar.get(Calendar.YEAR);
         int month = calendar.get(Calendar.MONTH) + 1;
@@ -43,15 +43,21 @@ public class Run {
             monthString = "0" + month;
         }
         String base = "/videos/" + year + "-" + monthString + "/" + videoId + "/";
+        //上传m3u8文件
+        File m3u8File = makeM3u8Result.getM3u8File();
         String m3u8FileUrl = BaiduCloudUtil.uploadObjectStorage(
                 m3u8File, base + m3u8File.getName());
         makeM3u8Result.setM3u8FileUrl(m3u8FileUrl);
+        //上传ts碎片
         List<File> tsFileList = makeM3u8Result.getTsFileList();
         List<String> tsFileUrlList = new ArrayList<>();
-        for (int i = 0; i < tsFileList.size(); i++) {
+        int total = tsFileList.size();
+        for (int i = 0; i < total; i++) {
             File file = tsFileList.get(i);
             String tsFileUrl = BaiduCloudUtil.uploadObjectStorage(file, base + file.getName());
-            System.out.println(tsFileUrl);
+            int current = i + 1;
+            int progress = (int) (current * 1.0 / total * 100);
+            System.out.println(progress + "% " + tsFileUrl);
             tsFileUrlList.add(i, tsFileUrl);
         }
         makeM3u8Result.setTsFileUrlList(tsFileUrlList);
