ceed638e4ae713cb122f2b88d32e25f8a9790c6a
makewheels <makewheels>
2021-01-08 00:18:06 +0800

    localhost

--- src/main/java/com/eg/videoosandserver/prepare/Run.java
+++ src/main/java/com/eg/videoosandserver/prepare/Run.java
@@ -98,7 +98,7 @@ public class Run {
     public static void main(String[] args) {
         //是否转码为720p
         boolean transcodeTo720p = false;
-        String videoFilePath = "C:\\Users\\thedoflin\\Videos\\Desktop\\Desktop 2021.01.05 - 22.35.27.02.mp4";
+        String videoFilePath = "C:\\Users\\thedoflin\\Videos\\Desktop\\Desktop 2021.01.08 - 00.11.26.01.mp4";
         //原始视频
         File originalVideoFile = new File(videoFilePath);
         //最终要转m3u8上传的视频
