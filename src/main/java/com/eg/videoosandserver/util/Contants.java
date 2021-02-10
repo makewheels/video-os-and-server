aaf13dbd220d105413c61b0f5bac77430e433b73
bq <gOtQWGG4wZ>
2021-02-10 16:48:45 +0800

    www.java8.icu

--- src/main/java/com/eg/videoosandserver/util/Contants.java
+++ src/main/java/com/eg/videoosandserver/util/Contants.java
@@ -3,6 +3,6 @@ package com.eg.videoosandserver.util;
 public class Contants {
 //    public static String IP = "localhost";
 //    public static String IP = "baidu.server.qbserver.cn";
-    public static String IP = "java8.icu";
+    public static String IP = "www.java8.icu";
 
 }
