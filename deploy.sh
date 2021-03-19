8566c3d988462671997dbfd0940c76178852b43c
publishcode <tongbuwangpan@163.com>
2021-03-19 22:51:47 +0800

    -Dspring.profiles.active=product

--- deploy.sh
+++ deploy.sh
@@ -49,7 +49,7 @@ health_check() {
 }
 start_application() {
     echo "starting java process"
-    nohup java -jar ${JAR_NAME} -Dspring.profiles.active=product > ${JAVA_OUT} 2>&1 &
+    nohup java -jar -Dspring.profiles.active=product ${JAR_NAME} > ${JAVA_OUT} 2>&1 &
     echo "started java process"
 }
 
