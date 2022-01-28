6469b1d9755b0e5ba3b2544b227ca3c074235472
binqiao <you@example.com>
2022-01-28 10:28:51 +0800

    返回前端页面

--- src/main/java/com/eg/videoosandserver/util/Constants.java
+++ src/main/java/com/eg/videoosandserver/util/Constants.java
@@ -1,7 +1,5 @@
 package com.eg.videoosandserver.util;
 
-import cn.hutool.core.io.file.FileNameUtil;
-
 public class Constants {
     private static final String IP = "www.itube.work";
     public static final String BASE_URL = "https://" + IP;
